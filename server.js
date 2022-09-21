99bd66f2be0acd052f1edbf66f896816401f23a1
tanmay7256 <tanmayarora1112@gmail.com>
2022-09-21 15:36:24 +0530

    for clever

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // const { info } = require('console');
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
